99a1a261b948f9ea5beda85da7009bb8511c8184
cyber9ja <ayodejioladapo15@gmail.com>
2025-02-28 19:41:58 +0000

    npm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
+import { AuthenticationController } from './authentication/authentication.controller';
+import { AuthenticationService } from './authentication/authentication.service';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule, UserModule, PaymentModule],
+  controllers: [AppController, AuthenticationController],
+  providers: [AppService, AuthenticationService],
 })
 export class AppModule {}
