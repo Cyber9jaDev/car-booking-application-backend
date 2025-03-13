38681f8d7af4cf468975a0e129d070c699d8115c
cyber9ja <ayodejioladapo15@gmail.com>
2025-03-13 03:41:25 +0000

    Quick fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
-import { UserModule } from './user/user.module';
-import { AuthenticationController } from './authentication/authentication.controller';
-import { AuthenticationService } from './authentication/authentication.service';
-import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, PaymentModule],
-  controllers: [AppController, AuthenticationController],
-  providers: [AppService, AuthenticationService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
