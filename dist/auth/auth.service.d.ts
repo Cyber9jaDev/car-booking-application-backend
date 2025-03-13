6e964031057e58b11af2644c0ae3dab0982a047c
cyber9ja <ayodejioladapo15@gmail.com>
2025-03-13 03:53:43 +0000

    installed necessary libraries

---
+++ dist/auth/auth.service.d.ts
@@ -0,0 +1,10 @@
+import { CreateAuthDto } from './dto/create-auth.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+export declare class AuthService {
+    signup(): Promise<{}>;
+    create(createAuthDto: CreateAuthDto): string;
+    findAll(): string;
+    findOne(id: number): string;
+    update(id: number, updateAuthDto: UpdateAuthDto): string;
+    remove(id: number): string;
+}
