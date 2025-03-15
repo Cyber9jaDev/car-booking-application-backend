495e61c2506e6f5411ab1dc4d9ccac25d4f18a83
cyber9ja <ayodejioladapo15@gmail.com>
2025-03-15 23:53:27 +0000

    modified auth guard

--- src/guard/auth.guard.ts
+++ src/guard/auth.guard.ts
@@ -1,12 +1,13 @@
 import {
   CanActivate,
   ExecutionContext,
+  ForbiddenException,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
-import { Role } from '@prisma/client';
+import { Role, User } from '@prisma/client';
 import { Request } from 'express';
 import { TokenPayload } from 'src/auth/types/auth.types';
 import { DatabaseService } from 'src/database/database.service';
@@ -20,49 +21,83 @@ export class AuthGuard implements CanActivate {
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    // "roles" is coming from the Roles decorator metadata
-    const requiredRoles: Role[] = await this.reflector.getAllAndOverride(
-      'roles',
-      [context.getHandler(), context.getClass()],
-    );
-
-    // If no role is provided
-    if (!requiredRoles || requiredRoles.length === 0) {
-      return true;
-    }
-
-    const request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request);
-
-    if (!token) {
-      throw new UnauthorizedException('No token provided');
-    }
-
     try {
-      const payload = (await this.jwtService.verifyAsync(token, {
-        secret: process.env.JWT_KEY,
-      })) as TokenPayload;
+      const requiredRoles = this.getRequiredRoles(context); // "roles" is coming from the Roles decorator metadata
 
-      const user = await this.database.user.findUnique({
-        where: { id: payload.userId },
-        select: { id: true, role: true },
-      });
-
-      if (!user) {
-        throw new UnauthorizedException('User does not exist');
+      // If no roles are required, allow access (Public route);
+      if (this.isPublicRoute(requiredRoles)) {
+        return true;
       }
+      const request = this.getRequest(context);
+      const accessToken = this.extractTokenFromCookie(request);
 
-      // We're assigning the payload to the request object here so that we can access it in our route handlers
-      request.user = payload;
+      // if (!accessToken) {
+      //   throw new UnauthorizedException('No token provided');
+      // }
+      const TokenPayload = await this.validateToken(accessToken);
+      const authenticateduser = await this.validateUser(TokenPayload);
+      // Attach user to request for use in controllers
+      request.user = TokenPayload;
 
-      return requiredRoles.includes(user.role); //true
+      return this.hasRequiredRole(requiredRoles, authenticateduser.role);
     } catch (error) {
       throw new UnauthorizedException('Invalid token');
     }
   }
 
-  private extractTokenFromHeader(request: Request): string | undefined {
-    const [type, token] = request.headers.authorization?.split(' ') ?? [];
-    return type === 'Bearer' ? token : undefined;
+  private hasRequiredRole(requiredRoles: Role[], userRole: Role): boolean {
+    const hasPermission = requiredRoles.includes(userRole);
+    if (!hasPermission) {
+      throw new ForbiddenException(
+        'You do not have permission to access this resource',
+      );
+    }
+    return true;
+  }
+
+  private async validateUser(payload: TokenPayload) {
+    const user = await this.database.user.findUnique({
+      where: { id: payload.userId },
+      select: { id: true, role: true },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('User does not exist');
+    }
+
+    return user;
+  }
+
+  private async validateToken(token: string): Promise<TokenPayload> {
+    try {
+      return await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_KEY,
+      });
+    } catch (error) {
+      throw new UnauthorizedException('Invalid or expired token');
+    }
+  }
+
+  private isPublicRoute(requiredRoles: Role[]): boolean {
+    return !requiredRoles || requiredRoles.length === 0;
+  }
+
+  private getRequiredRoles(context: ExecutionContext) {
+    return this.reflector.getAllAndOverride<Role[]>('roles', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+  }
+
+  private getRequest(context: ExecutionContext): Request {
+    return context.switchToHttp().getRequest<Request>();
+  }
+
+  private extractTokenFromCookie(request: Request) {
+    const token = request.cookies['access-token'];
+    if (!token) {
+      throw new UnauthorizedException('Authentication token is missing');
+    }
+    return token;
   }
 }
--- src/interceptor/user.interceptor.ts
+++ src/interceptor/user.interceptor.ts
@@ -1,4 +1,10 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor, UnauthorizedException } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
@@ -7,9 +13,9 @@ import { Observable } from 'rxjs';
 export class UserInterceptor implements NestInterceptor {
   constructor(private readonly jwtService: JwtService) {}
 
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any>  {
-    const request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request);
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request: Request = context.switchToHttp().getRequest();
+    const token = request.cookies['access-token'];
 
     if (!token) {
       throw new UnauthorizedException('No token provided');
@@ -17,15 +23,11 @@ export class UserInterceptor implements NestInterceptor {
 
     try {
       const user = this.jwtService.decode(token);
+      console.log("Interceptor", user);
       request.user = user;
     } catch (error) {
       throw new UnauthorizedException('Invalid token');
     }
     return next.handle();
-  }
-
-  private extractTokenFromHeader(request: Request): string | undefined {
-    const [type, token] = request?.headers?.authorization?.split(' ') ?? [];
-    return type === 'Bearer' ? token : undefined;
   }
 }
