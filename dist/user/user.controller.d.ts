33b9c3e3d0c013bd36e42c2d31835d981efc0572
cyber9ja <ayodejioladapo15@gmail.com>
2025-03-16 00:50:25 +0000

    ...

--- dist/user/user.controller.d.ts
+++ dist/user/user.controller.d.ts
@@ -4,8 +4,8 @@ export declare class UserController {
     private readonly userService;
     constructor(userService: UserService);
     getAuthUser(request: Request): Promise<{
-        id: string;
         name: string;
+        id: string;
         email: string;
         role: import(".prisma/client").$Enums.Role;
     } | null>;
--- dist/user/user.service.d.ts
+++ dist/user/user.service.d.ts
@@ -4,8 +4,8 @@ export declare class UserService {
     private readonly database;
     constructor(database: DatabaseService);
     getAuthUser(request: Request): Promise<{
-        id: string;
         name: string;
+        id: string;
         email: string;
         role: import(".prisma/client").$Enums.Role;
     } | null>;
